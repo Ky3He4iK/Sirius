8d9ca464236cfd8fafb3b93b58f2ab5de463b114
ky3he4ik <misha10311@yandex.ru>
2018-11-20 14:35:37 +0300

    result.html done

--- Api/__init__.py
+++ Api/__init__.py
@@ -86,12 +86,6 @@ def adv_search(request):
     return HttpResponse(Core.get_schools_filter_json(request.POST['data']), content_type="application/json")
 
 
-def adv_search(request):
-    if request.method != 'POST' or "data" not in request.POST:
-        return HttpResponse("I'm a teapot, I can't understand you", content_type="text/plain", status=418)
-    return HttpResponse(Core.get_schools_filter_json(request.POST['data']), content_type="application/json")
-
-
 def search(request):
     if "s" not in request.GET:
         return HttpResponse("I'm a teapot, I can't understand you", content_type="text/plain", status=418)
